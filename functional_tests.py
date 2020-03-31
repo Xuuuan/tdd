fd4aa4c8bc2cb79b342c621f9c0c2fd7524cfebd
Xuuuan <1808485793@qq.com>
2020-03-31 11:19:10 +0800

    First unit test and url mapping, dummy view

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,46 @@
-from selenium import webdriver #(1)
+from selenium import webdriver       
+import unittest
 
-browser = webdriver.Firefox()  #(2)
-browser.get('http://localhost:8000')#(3)
+class NewVisitorTest(unittest.TestCase): #(1)
+    
+    def setUp(self):#(3)
+        self.browser=webdriver.Firefox()
 
-assert 'Django' in browser.title #(4)
+    def tearDown(self):#(3)
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self): #(2)
+        # Edith has heard about a cool new online to-do app.She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn( 'To-Do',self.browser.title)#(4)
+        self.fail('Finish the test!')#(5)
+        
+            
+
+# She is invited to enter a to-do item straight away
+
+# She types "But peacock feathers" into a text box (Edith's hobby
+# is trying fly-fishing lures)
+
+# when she hits enter, the page updates, and now the page lists
+# "1: Buy a peacock feathers" as an item in a to-do list
+
+# There is stiil a text box inviting her to add another item. She
+# enters "Use peacock feathers to make a fly"(Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Edith wonders whether the site will remember her list. Then she sees
+# that the site has generated a unique URL for her -- there is some
+# explanatory text to that effect.
+
+# She visits that URL -her to-do list is stiil there.
+
+# Satisfied, she goes back to sleep
+if __name__=='__main__':#(6)
+    unittest.main(warnings='ignore')#(7)
+
+browser.quit()
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.http import HttpRequest
+from lists.views import home_page #(2)
 
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1+1,3)
+class HomePageTest(TestCase):
+    
+    def test_root_url_resolve_to_home_page_view(self):
+        found=resolve('/') #(1)
+        self.assertEqual(found.func,home_page)#(1)
+
+    def test_home_page_returns_correct_html(self):
+        request=HttpRequest()#1
+        response
+        
 # Create your tests here.
