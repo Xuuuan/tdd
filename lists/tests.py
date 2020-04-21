021a7726dfa789e122ef5fff7166341aa060a0f1
Xuuuan <1808485793@qq.com>
2020-04-21 11:05:07 +0800

    aaaa

--- lists/tests.py
+++ lists/tests.py
@@ -14,5 +14,15 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response=self.client.get('/')#1
         self.assertTemplateUsed(response,'home.html')#3
+
+    def test_uses_home_template(self):
+        response=self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response=self.client.post('/',data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response,'home.html')
+        
         
 
